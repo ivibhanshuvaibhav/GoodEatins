04db2511f4fc9eff864a8594dd6f6aea6f9565b8
Vibhanshu Vaibhav <vibhanshu_chaudhary@yahoo.co.in>
2018-12-08 22:01:04 +0530

    Initial commit

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by Vibhanshu Vaibhav on 02/12/18.
+//  Copyright © 2018 Vibhanshu Vaibhav. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title:  String
+    let imageName: String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+ //
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by Vibhanshu Vaibhav on 02/12/18.
+//  Copyright © 2018 Vibhanshu Vaibhav. All rights reserved.
+//
+
+import Foundation
+
+ struct Recipe {
+    let title: String
+    let instructions: String
+    let imageName: String
+ }
